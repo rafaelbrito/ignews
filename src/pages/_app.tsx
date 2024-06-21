f58f636e0a892b1ca40ee6d823fe19764f92df35
Rafael Brito <rafaelbrito@live.com>
2024-06-21 19:05:26 -0400

    Commit Inicial

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from "next/app";
+import '../styles/global.scss';
+import { Header } from "../components/Header";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
+}
